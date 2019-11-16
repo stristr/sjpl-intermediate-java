eed36aa6aa898f9a997ee4283575f1cf9a6e6061
Xinru <xinrulin99@gmail.com>
2019-11-15 20:03:41 -0800

    added animal

---
+++ Animal/Bear.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+/**
+ * This creates an animal with s set max amount of energy. Animal object can move and
+ * eat.
+ *
+ * @author Xinru Lin
+ * @version 11/29/2017
+ */
+
+
+public class Bear extends Animal
+{
+    private int max;
+    /**
+     * this creates an animal object
+     * @param maxEnergy is the the maximum energy animal can have
+     */
+    public Bear(int maxEnergy)
+    {
+        super();
+        max = maxEnergy;
+    }
+
+    /**
+     * this increases the energy of animal
+     * @param amount is that amount of energy gained
+     */
+    public void eat(int amount)
+    {
+        if( this.getEnergy() < max && amount < max)
+        {
+            super.eat(amount);
+        }
+        if(amount >  max)
+        {
+            super.eat(max - this.getEnergy());
+        }
+    }
+    
+       /**
+     * this increases the energy of animal
+     * @param amount is that amount of energy gained
+     */
+    public void speak(int length){
+       String[] words  = {"rawr!!!", "Give me honey!!", "I want fish"};
+       int i = 0;
+       Random rand = new Random();
+       int randomNumber = rand.nextInt(3);
+       while(i < length){
+           System.out.println(words[randomNumber]);
+        }
+    }
+}
---
+++ Animal/Chatting.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+/**
+ * Write a description of class Chatting here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Chatting
+{
+    /**
+     * Constructor for objects of class Chatting
+     */
+    public Chatting()
+    {
+        // initialise instance variables
+       
+    }
+
+    public static void Main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        while (s != "stop"){
+            if(
+        }
+    }
+        
+    }
+}
